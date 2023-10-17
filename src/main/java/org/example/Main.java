456c8dc7054a8933336dbb4ecffecbb8d2ee3aaf
Natsuminmin <basnatubadlove_misuse@outlook.jp>
2023-10-17 23:20:47 +0900

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("takagi");
+        names.add("abe");
+        names.add("kiki");
+        names.add("watanabe");
+        names.add("ono");
+
+        List result = names.stream().map(name -> name.toUpperCase()).filter(name -> name.contains("A")).toList();
+        System.out.println(result);
+
+        List<String> dogs = List.of("ai", "akua", "kaiji", "haruhi", "takuya");
+        List dog = dogs.stream().sorted().filter(String -> String.length() >= 5).toList();
+        System.out.println(dog);
+
+
+    }
+}
+
+
