1a5591624ec992eac808f3d62cb912962cc12159
Natsuminmin <basnatubadlove_misuse@outlook.jp>
2023-10-21 20:50:07 +0900

    修正しました

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,19 @@ import java.util.List;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
+        List<String> names = new ArrayList<String>();
         names.add("takagi");
         names.add("abe");
         names.add("kiki");
         names.add("watanabe");
         names.add("ono");
 
-        List result = names.stream().map(name -> name.toUpperCase()).filter(name -> name.contains("A")).toList();
+        List<String> result = names.stream().map(name -> name.toUpperCase()).filter(name -> name.contains("A")).toList();
         System.out.println(result);
 
-        List<String> dogs = List.of("ai", "akua", "kaiji", "haruhi", "takuya");
-        List dog = dogs.stream().sorted().filter(String -> String.length() >= 5).toList();
+
+        List<String> dogs = List.of("ai", "akua", "kaiji", "haruhi", "takuya", "jon");
+        List<String> dog = dogs.stream().sorted().filter(String -> String.length() >= 5).toList();
         System.out.println(dog);
 
 
